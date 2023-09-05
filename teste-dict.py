ea8f0032c4e8596b3e458cfa634f7c5623db69e9
Victor <victormnuzzi003@gmail.com>
2023-09-05 15:41:22 -0300

    first commit | arquivo do sprint2 adicionado | Menu Verificar Lixeiras adicionado | Lixeiras adicionadas na Operação Realizada | .py com dicionario das lixeiras

---
+++ teste-dict.py
@@ -0,0 +1,26 @@
+lixeiras = {
+    'lixeira1': {
+    'ID': '101',
+    'local': 'Rua A, Número 123',
+    'status_coleta': 'Pendente',
+    'enchimento': 85,
+    'ultima_coleta': '2023-09-01 10:30:00',
+    'responsavel': 'Equipe A'
+    },
+    'lixeira2': {
+        'ID': '202',
+        'local': 'Rua B, Número 456',
+        'status_coleta': 'Concluída',
+        'enchimento': 40,
+        'ultima_coleta': '2023-08-28 15:45:00',
+        'responsavel': 'Equipe H'
+    },
+    'lixeira3': {
+        'ID': '303',
+        'local': 'Rua C, Número 789',
+        'status_coleta': 'Pendente',
+        'enchimento': 95,
+        'ultima_coleta': '2023-09-03 09:15:00',
+        'responsavel': 'Equipe A'
+    }
+}
